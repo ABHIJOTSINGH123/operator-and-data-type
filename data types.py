8cde1f3019168823aecb3223c2d41c0251714604
Abhijot singh <manjeetkaur2w00786@gmail.com>
2024-11-16 07:22:31 -0800

    python

---
+++ data types.py
@@ -0,0 +1,22 @@
+name="Abhijot singh"
+age=13
+student=True
+weight=70.0
+print("name =",name)
+print("datatype of name is",type(name))
+print("age =",age)
+print("datatype of age is",type(age))
+print("student =",student)
+print("datatype of student is",type(student))
+print("weight =",weight)
+print("datatype of weight is",type(weight))
+#data typecasting
+print("after typecasting")
+age=str(age)
+print(age)
+print("data type of age is",type(age))
+weight=int(weight)
+print(weight)
+print("data type of weight is",type(weight))
+age=int(age)
+print("datatype of age is",type(age))
