7809c50d2018558d878b2d2ed6a8146a2a576751
Abhijot singh <manjeetkaur2w00786@gmail.com>
2024-11-16 07:38:46 -0800

    py

---
+++ operator.py
@@ -0,0 +1,20 @@
+num1=45
+num2=2
+print ("num1=",num1)
+print("num2 =",num2)
+print("addition=",num1+num2)
+print("substraction=",num1-num2)
+print("multiplication=",num1*num2)
+print("division=",num1/num2)
+print("floordivision=",num1//num2)
+print("modulous=",num1%num2)
+print("square",num2**2)
+print("square root=",num1**0.5)
+print("equal ?",num1==num2)
+print("num1 is greater?",num1>=num2)
+print("num2 is greater?",num1<=num2)
+print("num1 is not equal?",num1!=num2)
+result=num1/2+num2**2+10
+print("num1/2+num2**2+10")
+print("result of given equation =",result)
+
